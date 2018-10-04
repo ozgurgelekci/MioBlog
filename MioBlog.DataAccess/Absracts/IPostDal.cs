e50379f73e33081defab3e61613622bb0950eda7
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 17:12:09 +0300

    post data access layer interface

---
+++ MioBlog.DataAccess/Absracts/IPostDal.cs
@@ -0,0 +1,15 @@
+﻿using MioBlog.Core.DataAccess.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Entities.Concrete;
+
+namespace MioBlog.DataAccess.Absracts
+{
+    public interface IPostDal : IEntityRepository<Post>
+    {
+
+    }
+}
