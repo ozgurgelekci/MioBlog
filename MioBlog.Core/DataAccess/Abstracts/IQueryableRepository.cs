c6e399d09e3afece4be4768ec9ca4f2cb9797527
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 16:23:09 +0300

    IQueryable interface for select functions

---
+++ MioBlog.Core/DataAccess/Abstracts/IQueryableRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Core.Entities;
+
+namespace MioBlog.Core.DataAccess.Abstracts
+{
+    public interface IQueryableRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        IQueryable<TEntity> Table { get; set; }
+    }
+}
