f21ee28d87bdbfbc81289fc39659e56b7e077a26
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 18:25:02 +0300

    update to file name

--- MioBlog.DataAccess/Absracts/IPostCategoryDal.cs
+++ MioBlog.DataAccess/Absracts/IPostCategoryDal.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MioBlog.Core.DataAccess.Abstracts;
-using MioBlog.Entities.Concrete;
+using MioBlog.Entities.Concretes;
 
 namespace MioBlog.DataAccess.Absracts
 {
