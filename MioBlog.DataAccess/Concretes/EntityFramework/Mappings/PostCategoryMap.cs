30ada634b48c3a9a2cb2d2477e9ac19bd135d0b1
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 17:35:14 +0300

    post category db map

---
+++ MioBlog.DataAccess/Concretes/EntityFramework/Mappings/PostCategoryMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Entities.Concrete;
+
+namespace MioBlog.DataAccess.Concretes.EntityFramework.Mappings
+{
+   public class PostCategoryMap:EntityTypeConfiguration<PostCategory>
+    {
+        public PostCategoryMap()
+        {
+            ToTable(@"PostCategories", @"dbo");
+            HasKey(p => p.PostCategoryId);
+
+            Property(p => p.PostCategoryId).HasColumnName("PostCategoryId");
+            Property(p => p.PostCategoryName).HasColumnName("PostCategoryName");
+            Property(p => p.InsertedDate).HasColumnName("InsertedDate");
+        }
+    }
+}
