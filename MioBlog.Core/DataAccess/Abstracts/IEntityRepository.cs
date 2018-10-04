265595a8cafd0354ba7798ef17a8ae8bbf9f16e2
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 16:21:04 +0300

    general entity interface

---
+++ MioBlog.Core/DataAccess/Abstracts/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Core.Entities;
+
+namespace MioBlog.Core.DataAccess.Abstracts
+{
+    public interface IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter = null);
+        TEntity Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Delete<T>(TEntity entity);
+    }
+}
