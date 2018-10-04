9fb0b6a3184d5e969382c9e8f49ec2e8dac8d4cd
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 17:03:57 +0300

    post entity

---
+++ MioBlog.Entities/Concrete/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Core.Entities;
+
+namespace MioBlog.Entities.Concrete
+{
+    public class Post : IEntity
+    {
+        public virtual int PostId { get; set; }
+        public virtual int PostCategoryId { get; set; }
+        public virtual string PostTitle { get; set; }
+        public virtual string PostSummary { get; set; }
+        public virtual string PostContent { get; set; }
+        public virtual string CoverPic { get; set; }
+        public virtual DateTime InsertedDate { get; set; }
+
+        public PostCategory PostCategory { get; set; }
+    }
+}
