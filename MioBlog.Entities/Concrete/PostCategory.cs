fb45e9fbe05496f198f10187fb1022537fe6344c
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 17:04:09 +0300

    post category entity

---
+++ MioBlog.Entities/Concrete/PostCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MioBlog.Entities.Concrete
+{
+   public class PostCategory
+    {
+        public int PostCategoryId { get; set; }
+        public string PostCategoryName { get; set; }
+        public DateTime  InsertedDate { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
