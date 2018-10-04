b2740d3a09f68c4fc483b16e0d61a920e032a766
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 18:27:20 +0300

    context file

---
+++ MioBlog.DataAccess/Concretes/EntityFramework/MioContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.DataAccess.Concretes.EntityFramework.Mappings;
+using MioBlog.Entities.Concretes;
+
+namespace MioBlog.DataAccess.Concretes.EntityFramework
+{
+    public class MioContext : DbContext
+    {
+        public MioContext() : 
+            base("data source=.; initial catalog=MioBlogDB; integrated security=true;")
+        {
+            Database.SetInitializer<MioContext>(null);
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new PostCategoryMap());
+            modelBuilder.Configurations.Add(new PostMap());
+        }
+    }
+}
