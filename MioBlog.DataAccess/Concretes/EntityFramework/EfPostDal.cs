b5717352961478d6f2dc32b9ea2474fe7c8d4ccc
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 18:36:31 +0300

    post concrete class

---
+++ MioBlog.DataAccess/Concretes/EntityFramework/EfPostDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Core.DataAccess.Concretes.EntityFramework;
+using MioBlog.DataAccess.Absracts;
+using MioBlog.Entities.Concretes;
+
+namespace MioBlog.DataAccess.Concretes.EntityFramework
+{
+    public class EfPostDal : EfEntityRepositoryBase<Post, MioContext>, IPostDal
+    {
+        //other posts methods
+    }
+}
