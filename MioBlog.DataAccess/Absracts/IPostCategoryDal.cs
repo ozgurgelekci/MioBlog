ee7451a480fcb6239a00fc6e6caccf810c5e3327
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 17:11:59 +0300

    post category data access layer interface

---
+++ MioBlog.DataAccess/Absracts/IPostCategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Core.DataAccess.Abstracts;
+using MioBlog.Entities.Concrete;
+
+namespace MioBlog.DataAccess.Absracts
+{
+    public interface IPostCategoryDal : IEntityRepository<PostCategory>
+    {
+
+    }
+}
