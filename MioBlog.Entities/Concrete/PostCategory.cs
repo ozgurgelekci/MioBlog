7a6f0c3970a1c09b57f422416f698a544bbbdff4
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-04 17:11:23 +0300

    add IEntity

--- MioBlog.Entities/Concrete/PostCategory.cs
+++ MioBlog.Entities/Concrete/PostCategory.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MioBlog.Core.Entities;
 
 namespace MioBlog.Entities.Concrete
 {
-   public class PostCategory
+    public class PostCategory : IEntity
     {
         public int PostCategoryId { get; set; }
         public string PostCategoryName { get; set; }
-        public DateTime  InsertedDate { get; set; }
+        public DateTime InsertedDate { get; set; }
 
         public ICollection<Post> Posts { get; set; }
     }
