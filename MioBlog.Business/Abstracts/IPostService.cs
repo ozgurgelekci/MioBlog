81533ef7450ac46488f49741efdc4502732368ad
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-05 14:01:06 +0300

    post service interfaces

---
+++ MioBlog.Business/Abstracts/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MioBlog.Entities.Concretes;
+
+namespace MioBlog.Business.Abstracts
+{
+    public interface IPostService
+    {
+        List<Post> GetAll();
+        List<Post> GetAllWithCategoryId(int postCategoryId);
+        Post GetById(int postId);
+        Post Add(Post post);
+        Post Update(Post post);
+        void Delete(Post post);
+    }
+}
