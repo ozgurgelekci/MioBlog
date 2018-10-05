72b8d931c817c4e088c3d89b2af70c2702c31f9f
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-05 13:05:09 +0300

    validation rules

---
+++ MioBlog.Business/ValidationRules/FluentValidation/PostCategoryValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MioBlog.Entities.Concretes;
+
+namespace MioBlog.Business.ValidationRules.FluentValidation
+{
+    public class PostCategoryValidator : AbstractValidator<PostCategory>
+    {
+        public PostCategoryValidator()
+        {
+            RuleFor(p => p.PostCategoryName).NotEmpty().MaximumLength(20);
+        }
+    }
+}
---
+++ MioBlog.Business/ValidationRules/FluentValidation/PostValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MioBlog.Entities.Concretes;
+
+namespace MioBlog.Business.ValidationRules.FluentValidation
+{
+    public class PostValidator : AbstractValidator<Post>
+    {
+        public PostValidator()
+        {
+            RuleFor(p => p.PostCategoryId).NotEmpty().GreaterThan(0);
+            RuleFor(p => p.PostTitle).NotEmpty().MaximumLength(100);
+            RuleFor(p => p.PostContent).NotEmpty();
+            RuleFor(p => p.PostSummary).NotEmpty().MaximumLength(250);
+        }
+    }
+}
