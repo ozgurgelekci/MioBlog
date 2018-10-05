99d8e91da86f1b14ae8ba21085bf029bedbebb5c
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-05 15:39:20 +0300

    ninject implementation for dependency injection

---
+++ MioBlog.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MioBlog.Business.Abstracts;
+using MioBlog.Business.Concretes.Managers;
+using MioBlog.Core.DataAccess.Abstracts;
+using MioBlog.Core.DataAccess.Concretes.EntityFramework;
+using MioBlog.DataAccess.Abstracts;
+using MioBlog.DataAccess.Concretes.EntityFramework;
+using Ninject.Modules;
+
+namespace MioBlog.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPostService>().To<PostManager>().InSingletonScope();
+            Bind<IPostDal>().To<EfPostDal>();
+
+            Bind<IPostCategoryService>().To<PostCategoryManager>().InSingletonScope();
+            Bind<IPostCategoryDal>().To<EfPostCategoryDal>();
+
+            Bind(typeof(IQueryableRepository<>)).To(typeof(EfQueryableRepositoryBase<>));
+            Bind<DbContext>().To<MioContext>();
+
+        }
+    }
+}
---
+++ MioBlog.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MioBlog.Business.ValidationRules.FluentValidation;
+using MioBlog.Entities.Concretes;
+using Ninject.Modules;
+
+namespace MioBlog.Business.DependencyResolvers.Ninject
+{
+  public  class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Post>>().To<PostValidator>().InSingletonScope();
+            Bind<IValidator<PostCategory>>().To<PostCategoryValidator>().InSingletonScope();
+        }
+    }
+}
