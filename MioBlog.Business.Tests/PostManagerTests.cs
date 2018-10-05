f015aa8b8502436ccdde02946fa1fb395cebfb88
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-05 15:19:15 +0300

    remove using

--- MioBlog.Business.Tests/PostManagerTests.cs
+++ MioBlog.Business.Tests/PostManagerTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentValidation;
+﻿using FluentValidation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MioBlog.Business.Concretes.Managers;
 using MioBlog.DataAccess.Abstracts;
