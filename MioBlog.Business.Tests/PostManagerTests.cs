399a85bfa07ff94d26371bbef4e1149f77e233d1
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-05 14:01:59 +0300

    added validation check test

---
+++ MioBlog.Business.Tests/PostManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MioBlog.Business.Concretes.Managers;
+using MioBlog.DataAccess.Abstracts;
+using MioBlog.Entities.Concretes;
+using Moq;
+
+namespace MioBlog.Business.Tests
+{
+    [TestClass]
+    public class PostManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void post_validation_check()
+        {
+            Mock<IPostDal> mock = new Mock<IPostDal>();
+            PostManager postManager = new PostManager(mock.Object);
+
+            postManager.Add(new Post());
+        }
+    }
+}
