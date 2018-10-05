dcb21cb8994e2c18304afa904abc969ed7664495
Özgür Gelekçi <ozgurgelekci@outlook.com>
2018-10-05 15:19:37 +0300

    added post category validation check test

---
+++ MioBlog.Business.Tests/PostCategoryManagerTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MioBlog.Business.Concretes.Managers;
+using MioBlog.DataAccess.Abstracts;
+using MioBlog.Entities.Concretes;
+using Moq;
+
+namespace MioBlog.Business.Tests
+{
+    [TestClass]
+    public class PostCategoryManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void postCategory_validation_check()
+        {
+            Mock<IPostCategoryDal> mock = new Mock<IPostCategoryDal>();
+            PostCategoryManager postCategoryManager = new PostCategoryManager(mock.Object);
+
+            postCategoryManager.Add(new PostCategory());
+        }
+    }
+}
